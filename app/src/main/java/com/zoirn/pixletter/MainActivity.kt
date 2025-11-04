9a6c15b06a255e291734e31301ef8a558728e033
Роман Зорин <sgena170315@gmail.com>
2025-11-04 13:40:31 +0300

    Создание функции GreetingText и изменение некоторых деталей в кодЕ

--- app/src/main/java/com/zoirn/pixletter/MainActivity.kt
+++ app/src/main/java/com/zoirn/pixletter/MainActivity.kt
@@ -5,15 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.zoirn.pixletter.ui.theme.PixLetterTheme
 
 
@@ -24,30 +20,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             PixLetterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Yellow) {
-        Text(
-            text = "Hello, my name is $name!",
-            modifier = Modifier.padding(24.dp)
-        )
-    }
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun RoyalInvitationPreview() {
     PixLetterTheme {
-        Greeting("Roman")
     }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message
+    )
 }
