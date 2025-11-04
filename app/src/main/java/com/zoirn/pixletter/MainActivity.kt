3cd0db976fcdc596445a0f2dd056b042a770eceb
Роман Зорин <sgena170315@gmail.com>
2025-11-04 13:53:36 +0300

    Финальная функция  GreetingText

--- app/src/main/java/com/zoirn/pixletter/MainActivity.kt
+++ app/src/main/java/com/zoirn/pixletter/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zoirn.pixletter.ui.theme.PixLetterTheme
 
@@ -39,13 +45,22 @@ fun RoyalInvitationPreview() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 80.sp
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            textAlign = TextAlign.End,
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+        )
+    }
 }
