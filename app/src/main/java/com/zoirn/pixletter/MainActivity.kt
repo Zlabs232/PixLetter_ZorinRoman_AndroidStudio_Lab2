6e8c39793394474344879c0b1799c1109f1b812a
Роман Зорин <sgena170315@gmail.com>
2025-11-04 14:15:33 +0300

    Добавление картинки и работа с кодом(13-14шаг)

--- app/src/main/java/com/zoirn/pixletter/MainActivity.kt
+++ app/src/main/java/com/zoirn/pixletter/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.zoirn.pixletter
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zoirn.pixletter.ui.theme.PixLetterTheme
+
+private val Unit.pixelcasstle: Int
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +45,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RoyalInvitationPreview() {
     PixLetterTheme {
-        GreetingText(
+        GreetingImage(
             message = "Royal Decree!\nDear Denis\nHis Majesty the King\nInvites you to a great feast!\nCome to the castle before sunset,\notherwise, you risk losing your head!",
             from = "From King Arthur",
             modifier = Modifier.padding(8.dp)
@@ -70,4 +75,14 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
                 .align(Alignment.End)
         )
     }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.pixelcasstle)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+
 }
